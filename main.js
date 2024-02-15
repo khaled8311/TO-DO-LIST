fd15da6b25db7909590c6b040646c7d7fd75131f
khaled8311 <khaledeltwahy72@gmail.com>
2024-02-15 18:34:22 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let head = document.getElementById("head");
+let full =document.getElementById("full");
+head.innerHTML = "To Do LIST"
+
+head.style.cssText = `
+margin: 0 0 20px;
+font-weight: bold;
+font-size: 28px;
+text-align: center;
+background-color: black;
+font-style: oblique;
+color: white;
+border-radius: 10px;
+font
+`
+full.style.cssText = `
+background-color: #0579cf;
+`
+
+
